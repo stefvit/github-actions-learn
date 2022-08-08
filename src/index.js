1118af874088ce84725f274ff164bd0f7d084c18
Vitor Carra <vitor.carra@gmail.com>
2022-08-07 23:23:09 -0300

    create simple app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const multiplication = (a, b) => {
+    return a * b;
+}
+
+const sum = (a, b) => {
+    return a + b;
+}
+
+const get_full_name = (name, surname) => {
+    return name + ' ' + surname
+}
+
+module.exports = { multiplication, sum, get_full_name }
---
+++ src/index.test.js
@@ -0,0 +1,13 @@
+const myClass = require('./index.js')
+
+test("add 5 + 5 to be equal 10", () => {
+    expect(myClass.sum(5, 5)).toBe(10);
+});
+
+test("add 5 * 5 to be equal 25", () => {
+    expect(myClass.multiplication(5, 5)).toBe(25);
+});
+
+// test("return full name for (John, Rock) as John Rock", () => {
+//     expect(myClass.get_full_name("John", "Rock")).toBe("John Rock");
+// });
