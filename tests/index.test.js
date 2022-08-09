da6d15ef365761e5f7f88d81e95500f514786f5d
Vitor Carra <vitor.carra@gmail.com>
2022-08-09 18:41:22 -0300

    add one test

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,9 +4,9 @@ test("add 5 + 5 to be equal 10", () => {
     expect(myClass.sum(5, 5)).toBe(10);
 });
 
-// test("add 5 * 5 to be equal 25", () => {
-//     expect(myClass.multiplication(5, 5)).toBe(25);
-// });
+test("add 5 * 5 to be equal 25", () => {
+    expect(myClass.multiplication(5, 5)).toBe(25);
+});
 
 // test("return full name for (John, Rock) as John Rock", () => {
 //     expect(myClass.get_full_name("John", "Rock")).toBe("John Rock");
