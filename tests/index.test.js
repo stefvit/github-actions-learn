e5abf12908bff359893683dd713feb0271e65403
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2022-08-09 15:56:23 -0300

    test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,8 +1,8 @@
-const myClass = require('../src/index.js')
+const myClass = require("../src/index.js");
 
-test("add 5 + 5 to be equal 10", () => {
-    expect(myClass.sum(5, 5)).toBe(10);
-});
+// test("add 5 + 5 to be equal 10", () => {
+//     expect(myClass.sum(5, 5)).toBe(10);
+// });
 
 // test("add 5 * 5 to be equal 25", () => {
 //     expect(myClass.multiplication(5, 5)).toBe(25);
